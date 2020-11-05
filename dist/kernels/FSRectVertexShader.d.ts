b44ed2a728987390659c77b6e7966460e62ad13e
Amanda Ghassaei <amandaghassaei@gmail.com>
2020-11-04 19:46:38 -0800

    flipping y axis convention

--- dist/kernels/FSRectVertexShader.d.ts
+++ dist/kernels/FSRectVertexShader.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "\nprecision highp float;\nattribute vec2 position;\nvarying vec2 uv;\nvoid main() {\n\tuv = 0.5 * (position + 1.0);\n\tgl_Position = vec4(position, 0, 1);\n}\n";
+declare const _default: "\nprecision highp float;\nattribute vec2 position;\nvarying vec2 uv;\nvoid main() {\n\t// Calculate UV coordinates [0, 1] of canvas bounds.\n\tuv = 0.5 * (position + 1.0);\n\t// Flip y coordinate so that y increases vertically on screen.\n\tuv.y = 1.0 - uv.y;\n\n\tgl_Position = vec4(position, 0, 1);\n}\n";
 export default _default;
