140dbf6f579c0fa5fa97ed3d91c7be0f2a203c57
Amanda Ghassaei <amandaghassaei@gmail.com>
2020-11-05 16:58:47 -0800

    changes to uv

--- dist/kernels/DefaultVertexShader.js
+++ dist/kernels/DefaultVertexShader.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // Vertex shader for fullscreen quad.
-exports.default = "\nprecision mediump float;\nattribute vec2 aPosition;\nuniform vec2 u_scale;\nuniform vec2 u_translation;\nvarying vec2 uv;\nvoid main() {\n\t// Apply transformations.\n\tvec2 position = u_scale * aPosition + u_translation;\n\t// Calculate UV coordinates [0, 1] of canvas bounds.\n\tuv = 0.5 * (position + 1.0);\n\t// Calculate vertex position.\n\tgl_Position = vec4(position, 0, 1);\n}\n";
+exports.default = "\nprecision mediump float;\nattribute vec2 aPosition;\nuniform vec2 u_scale;\nuniform vec2 u_translation;\nvarying vec2 uv;\nvoid main() {\n\t// Calculate UV coordinates [0, 1].\n\tuv = 0.5 * (aPosition + 1.0);\n\t// Apply transformations.\n\tvec2 position = u_scale * aPosition + u_translation;\n\t// Calculate vertex position.\n\tgl_Position = vec4(position, 0, 1);\n}\n";
 //# sourceMappingURL=DefaultVertexShader.js.map
--- src/kernels/DefaultVertexShader.ts
+++ src/kernels/DefaultVertexShader.ts
@@ -6,10 +6,10 @@ uniform vec2 u_scale;
 uniform vec2 u_translation;
 varying vec2 uv;
 void main() {
+	// Calculate UV coordinates [0, 1].
+	uv = 0.5 * (aPosition + 1.0);
 	// Apply transformations.
 	vec2 position = u_scale * aPosition + u_translation;
-	// Calculate UV coordinates [0, 1] of canvas bounds.
-	uv = 0.5 * (position + 1.0);
 	// Calculate vertex position.
 	gl_Position = vec4(position, 0, 1);
 }
