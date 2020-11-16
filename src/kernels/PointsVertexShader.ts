2103d4137a10e2cc4ffc1c74382b6b0687296921
Amanda Ghassaei <amandaghassaei@gmail.com>
2020-11-15 22:02:50 -0800

    fixes

--- src/kernels/PointsVertexShader.ts
+++ src/kernels/PointsVertexShader.ts
@@ -6,7 +6,7 @@ precision highp int;
 // Cannot use int vertex attributes: https://stackoverflow.com/questions/27874983/webgl-how-to-use-integer-attributes-in-glsl
 attribute float aIndex; // Index of point.
 
-uniform sampler2D u_position; // Texture lookup with position data.
+uniform sampler2D u_positions; // Texture lookup with position data.
 uniform vec2 u_positionDimensions;
 uniform vec2 u_scale;
 uniform float u_pointSize;
@@ -26,7 +26,7 @@ void main() {
 	vec2 positionUV = vec2(modI(aIndex, u_positionDimensions.x), floor(floor(aIndex + 0.5) / u_positionDimensions.x)) / u_positionDimensions;
 
 	// Lookup vertex position.
-	vec2 position = texture2D(u_position, positionUV).xy * u_scale;
+	vec2 position = texture2D(u_positions, positionUV).xy * u_scale;
 
 	// Calculate a global uv for the viewport.
 	vUV = 0.5 * (position + 1.0);
