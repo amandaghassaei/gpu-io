c3c02e8ffab6f23cdc686c801146e6bb8a06c72a
Amanda Ghassaei <amandaghassaei@gmail.com>
2020-11-16 11:42:28 -0800

    fixes

--- src/kernels/PointsVertexShader.ts
+++ src/kernels/PointsVertexShader.ts
@@ -28,7 +28,7 @@ void main() {
 
 	// Calculate a global uv for the viewport.
 	// Lookup vertex position and scale to [0, 1] range.
-	vUV = 0.5 * texture2D(u_positions, vParticleUV).xy * u_scale + 1.0;
+	vUV = texture2D(u_positions, vParticleUV).xy * u_scale;
 
 	// Calculate position in [-1, 1] range.
 	vec2 position = vUV * 2.0 - 1.0;
