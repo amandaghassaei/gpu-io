da30bdc101eb74433358c1369397718eeb0c028e
amandaghassaei <amandaghassaei@gmail.com>
2021-03-30 00:09:45 -0700

    build

--- dist/kernels/PassThroughFragmentShader.d.ts
+++ dist/kernels/PassThroughFragmentShader.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "\nprecision highp float;\n\nvarying vec2 vUV;\nuniform sampler2D u_state;\n\nvoid main() {\n\tgl_FragColor = texture2D(u_state, vUV);\n}";
+declare const _default: "\nprecision highp float;\n\nvarying vec2 v_UV;\nuniform sampler2D u_state;\n\nvoid main() {\n\tgl_FragColor = texture2D(u_state, v_UV);\n}";
 export default _default;
