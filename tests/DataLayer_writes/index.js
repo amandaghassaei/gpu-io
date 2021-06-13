4956de2a60d5aa3ee9ab2a9ef2b93c78e300302a
amandaghassaei <amandaghassaei@gmail.com>
2021-06-13 11:23:34 -0700

    small changes

--- tests/DataLayer_writes/index.js
+++ tests/DataLayer_writes/index.js
@@ -26,6 +26,22 @@ requirejs([
 			} = options;
 			let input;
 			let NUM_EXTREMA = 0;
+			let NUM_TYPE_EXTREMA = 0;
+			let NUM_HALF_FLOAT_INT_EXTREMA = 0;
+			let NUM_FLOAT_INT_EXTREMA = 0;
+
+			const MIN_UNSIGNED_INT = 0;
+			const MIN_BYTE = -(2 ** 7);
+			const MAX_BYTE = 2 ** 8 - 1;
+			const MIN_SHORT = -(2 ** 15);
+			const MAX_SHORT = 2 ** 16 - 1;
+			const MIN_INT = -(2 ** 31);
+			const MAX_INT = 2 ** 31 - 1;
+			const MIN_FLOAT_INT = -16777216;
+			const MAX_FLOAT_INT = 16777216;
+			const MIN_HALF_FLOAT_INT = -2048;
+			const MAX_HALF_FLOAT_INT = -2048;
+
 			switch (TYPE) {
 				case HALF_FLOAT: {
 					input = new Float32Array(DIM_X * DIM_Y * NUM_ELEMENTS);
@@ -73,109 +89,145 @@ requirejs([
 				case UNSIGNED_BYTE: {
 					input = new Uint8Array(DIM_X * DIM_Y * NUM_ELEMENTS);
 					// Fill with uint8 data.
-					const MIN = 0;
-					const MAX = 2 ** 8 - 1;
-					for (let i = 0; i < input.length; i++) {
-						input[i] = i % (MAX + 1);
-					}
 					// Test extrema values.
-					NUM_EXTREMA = 4;
+					NUM_TYPE_EXTREMA = 4;
+					NUM_EXTREMA = NUM_TYPE_EXTREMA;
 					// Minimum values.
-					input[0] = MIN;
+					input[0] = MIN_UNSIGNED_INT;
 					input[1] = input[0] + 1;
 					// Maximum values.
-					input[2] = MAX;
+					input[2] = MAX_BYTE;
 					input[3] = input[2] - 1;
+
+					const length = input.length - NUM_EXTREMA;
+					for (let i = 0; i < length; i++) {
+						input[i + NUM_EXTREMA] = i % (MAX_BYTE + 1);
+					}
 					break;
 				}
 				case UNSIGNED_SHORT: {
 					input = new Uint16Array(DIM_X * DIM_Y * NUM_ELEMENTS);
-					// Fill with uint16 data.
-					const MIN = 0;
-					const MAX = 2 ** 16 - 1;
-					for (let i = 0; i < input.length; i++) {
-						input[i] = i % (MAX + 1);
-					}
+					// Fill with uint16 data.					
 					// Test extrema values.
-					NUM_EXTREMA = 4;
+					NUM_TYPE_EXTREMA = 4;
+					NUM_HALF_FLOAT_INT_EXTREMA = 2;
+					NUM_EXTREMA = NUM_TYPE_EXTREMA + NUM_HALF_FLOAT_INT_EXTREMA;
 					// Minimum values.
-					input[0] = MIN;
+					input[0] = MIN_UNSIGNED_INT;
 					input[1] = input[0] + 1;
 					// Maximum values.
-					input[2] = MAX;
+					input[2] = MAX_SHORT;
 					input[3] = input[2] - 1;
+					// Check that at least half float values are supported.
+					input[4] = MAX_HALF_FLOAT_INT;
+					input[5] = input[4] - 1;
+
+					const length = input.length - NUM_EXTREMA;
+					for (let i = 0; i < length; i++) {
+						input[i + NUM_EXTREMA] = i % (MAX_SHORT + 1);
+					}
 					break;
 				}
 				case UNSIGNED_INT: {
 					input = new Uint32Array(DIM_X * DIM_Y * NUM_ELEMENTS);
 					// Fill with uint32 data.
-					const MIN = 0;
-					const MAX = 2 ** 32 - 1;
-					for (let i = 0; i < input.length; i++) {
-						input[i] = i % (MAX + 1);
-					}
 					// Test extrema values.
-					NUM_EXTREMA = 4;
+					NUM_TYPE_EXTREMA = 4;
+					NUM_FLOAT_INT_EXTREMA = 2;
+					NUM_HALF_FLOAT_INT_EXTREMA = 2;
+					NUM_EXTREMA = NUM_TYPE_EXTREMA + NUM_HALF_FLOAT_INT_EXTREMA + NUM_FLOAT_INT_EXTREMA;
 					// Minimum values.
-					input[0] = MIN;
+					input[0] = MIN_UNSIGNED_INT;
 					input[1] = input[0] + 1;
 					// Maximum values.
-					input[2] = MAX;
+					input[2] = MAX_INT;
 					input[3] = input[2] - 1;
+					// Check that at least float values are supported.
+					input[4] = MAX_FLOAT_INT;
+					input[5] = input[4] - 1;
+					// Check that at least half float values are supported.
+					input[6] = MAX_HALF_FLOAT_INT;
+					input[7] = input[4] - 1;
+
+					const length = input.length - NUM_EXTREMA;
+					for (let i = 0; i < length; i++) {
+						input[i + NUM_EXTREMA] = i % (MAX_INT + 1);
+					}
 					break;
 				}
 				case BYTE: {
 					input = new Int8Array(DIM_X * DIM_Y * NUM_ELEMENTS);
 					// Fill with int8 data.
-					const MIN = -(2 ** 7);
-					const MAX = 2 ** 7 - 1;
-					for (let i = 0; i < input.length; i++) {
-						input[i] = (i - Math.floor(input.length / 2))
-					}
 					// Test extrema values.
-					NUM_EXTREMA = 4;
+					NUM_TYPE_EXTREMA = 4;
+					NUM_EXTREMA = NUM_TYPE_EXTREMA;
 					// Minimum values.
-					input[0] = MIN;
+					input[0] = MIN_BYTE;
 					input[1] = input[0] + 1;
 					// Maximum values.
-					input[2] = MAX;
+					input[2] = MAX_BYTE;
 					input[3] = input[2] - 1;
+
+					const length = input.length - NUM_EXTREMA;
+					for (let i = 0; i < length; i++) {
+						input[i + NUM_EXTREMA] = (i - Math.floor(length / 2));
+					}
 					break;
 				}
 				case SHORT: {
 					input = new Int16Array(DIM_X * DIM_Y * NUM_ELEMENTS);
 					// Fill with int16 data.
-					const MIN = -(2 ** 15);
-					const MAX = 2 ** 15 - 1;
-					for (let i = 0; i < input.length; i++) {
-						input[i] = (i - Math.floor(input.length / 2));
-					}
 					// Test extrema values.
-					NUM_EXTREMA = 4;
+					NUM_TYPE_EXTREMA = 4;
+					NUM_HALF_FLOAT_INT_EXTREMA = 4;
+					NUM_EXTREMA = NUM_TYPE_EXTREMA + NUM_HALF_FLOAT_INT_EXTREMA;
 					// Minimum values.
-					input[0] = MIN;
+					input[0] = MIN_SHORT;
 					input[1] = input[0] + 1;
 					// Maximum values.
-					input[2] = MAX;
+					input[2] = MAX_SHORT;
 					input[3] = input[2] - 1;
+					// Check that at least half float values are supported.
+					input[4] = MIN_HALF_FLOAT_INT;
+					input[5] = input[4] + 1;
+					input[6] = MAX_HALF_FLOAT_INT;
+					input[7] = input[6] - 1;
+
+					const length = input.length - NUM_EXTREMA;
+					for (let i = 0; i < length; i++) {
+						input[i + NUM_EXTREMA] = (i - Math.floor(length / 2));
+					}
 					break;
 				}
 				case INT: {
 					input = new Int32Array(DIM_X * DIM_Y * NUM_ELEMENTS);
 					// Fill with int32 data.
-					const MIN = -(2 ** 31);
-					const MAX = 2 ** 31 - 1;
-					for (let i = 0; i < input.length; i++) {
-						input[i] = (i - Math.floor(input.length / 2));
-					}
 					// Test extrema values.
-					NUM_EXTREMA = 4;
+					NUM_TYPE_EXTREMA = 4;
+					NUM_FLOAT_INT_EXTREMA = 4;
+					NUM_HALF_FLOAT_INT_EXTREMA = 4;
+					NUM_EXTREMA = NUM_TYPE_EXTREMA + NUM_HALF_FLOAT_INT_EXTREMA + NUM_FLOAT_INT_EXTREMA;
 					// Minimum values.
-					input[0] = MIN;
+					input[0] = MIN_INT;
 					input[1] = input[0] + 1;
 					// Maximum values.
-					input[2] = MAX;
+					input[2] = MAX_INT;
 					input[3] = input[2] - 1;
+					// Check that at least float values are supported.
+					input[4] = MIN_FLOAT_INT;
+					input[5] = input[4] + 1;
+					input[6] = MAX_FLOAT_INT;
+					input[7] = input[6] - 1;
+					// Check that at least half float values are supported.
+					input[8] = MIN_HALF_FLOAT_INT;
+					input[9] = input[8] + 1;
+					input[10] = MAX_HALF_FLOAT_INT;
+					input[11] = input[10] - 1;
+
+					const length = input.length - NUM_EXTREMA;
+					for (let i = 0; i < length; i++) {
+						input[i + NUM_EXTREMA] = (i - Math.floor(length / 2));
+					}
 					break;
 				}
 				default:
@@ -206,29 +258,52 @@ requirejs([
 			if (input.length !== output.length) {
 				return {
 					status: ERROR,
-					error: `Input and output arrays have unequal length: ${input.length}, ${output.length}.`,
+					error: `Input and output arrays have unequal length: expected length ${input.length}, got length ${output.length}.`,
 				};
 			}
 			let numMismatches = 0;
-			let numExtremaMismatches = 0;
+			let minMatch = Infinity;
+			let maxMatch = -Infinity;
+			let validRange = [0, 0];
+			let extremaMismatches = '';
 			for (let i = 0; i < input.length; i++) {
 				if (input[i] !== output[i]) {
 					if (i < NUM_EXTREMA) {
-						console.log(`extrema mismatch: ${input[i]}, ${output[i]}`);
-						numExtremaMismatches++;
-					} else numMismatches++;
+						extremaMismatches += `expected ${input[i]}, got ${output[i]}<br/>`;
+					} else {
+						numMismatches++;
+						// Reset range calculation.
+						if (maxMatch !== Infinity && maxMatch - minMatch > validRange[1] - validRange[0]) {
+							validRange = [minMatch, maxMatch];
+							minMatch = Infinity;
+							maxMatch = -Infinity;
+						}
+					}
+				} else if (i >= NUM_EXTREMA) {
+					minMatch = Math.min(output[i], minMatch);
+					maxMatch = Math.max(output[i], maxMatch);
 				}
+			}
+			if (maxMatch !== Infinity && maxMatch - minMatch > validRange[1] - validRange[0]) {
+				validRange = [minMatch, maxMatch];
+			}
+			if (numMismatches === input.length) {
+				return {
+					status: ERROR,
+					error: `All elements of output array do not match input values.`,
+				};
 			}
 			if (numMismatches) {
 				return {
-					status: ERROR,
-					error: `Input and output arrays have ${numMismatches} mismatched elements.`,
+					status: WARNING,
+					error: `Input and output arrays have ${numMismatches} mismatched elements, valid values found in range [${validRange[0]}, ${validRange[1]}].`,
 				};
 			}
-			if (numExtremaMismatches) {
+			if (extremaMismatches !== '') {
+				console.warn(extremaMismatches);
 				return {
 					status: WARNING,
-					error: `Large values not supported.`,
+					error: `Valid values found in range [${validRange[0]}, ${validRange[1]}], extrema not supported.`,
 				};
 			}
 			return {
@@ -242,8 +317,8 @@ requirejs([
 		}
 	}
 
-	const DIM_X = 100;
-	const DIM_Y = 100;
+	const DIM_X = 1000;
+	const DIM_Y = 1000;
 
 	const output = document.getElementById('output');
 
