0e9c8fe7cf5df61b2f7363dfdf29b7bad149d96d
amandaghassaei <amandaghassaei@gmail.com>
2021-07-25 01:24:26 -0700

    small fixes

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
 // const SingleColorFragShader = require('./kernels_1.0/SingleColorFragShader.glsl');
 // const PointsVertexShader = require('./kernels_1.0/PointsVertexShader.glsl');
 // const PointsVertexShaderWithDisplacement = require('./kernels_1.0/PointsVertexShaderWithDisplacement.glsl');
-// const SegmentVertexShader = require('./kernels_1.0/SegmentVertexShader.glsl');
+const SegmentVertexShader = require('./glsl_1/SegmentVertexShader.glsl');
 // const ZeroFragmentShader = require('./kernels_1.0/ZeroFragShader.glsl');
 
 export {
@@ -26,4 +26,5 @@ export {
 	LINEAR,
 	CLAMP_TO_EDGE,
 	REPEAT,
+	SegmentVertexShader,
 };
