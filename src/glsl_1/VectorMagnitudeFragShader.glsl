83d8dfeeb3fb176244267cdf81fffb6b980c521b
amandaghassaei <amandaghassaei@gmail.com>
2021-09-28 21:21:57 -0700

    fixes

--- src/glsl_1/VectorMagnitudeFragShader.glsl
+++ src/glsl_1/VectorMagnitudeFragShader.glsl
@@ -10,9 +10,9 @@ uniform sampler2D u_internal_data;
 
 void main() {
 	vec4 value = texture2D(u_internal_data, v_UV);
-	if (u_internal_numDimensions < 4) value.a = 0;
-	if (u_internal_numDimensions < 3) value.b = 0;
-	if (u_internal_numDimensions < 2) value.g = 0;
+	if (u_internal_numDimensions < 4) value.a = 0.0;
+	if (u_internal_numDimensions < 3) value.b = 0.0;
+	if (u_internal_numDimensions < 2) value.g = 0.0;
 	float mag = length(value);
 	gl_FragColor = vec4(mag * u_scale * u_color, 1);
 }
